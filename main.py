0074623db3749418c5bd2559613829dab9e6ab69
Faithfulness Alamu <vaguemail369@gmail.com>
2021-07-25 00:01:59 +0100

    feat: choose a repo list source based on cli flags
    If the '--filename' flag is specified, a file source is used,
    otherwise an API aware source is used.

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import click
 
+from api import github
+from os import getenv
+from os.path import isfile
+from util import list_repos_from_file
+from functools import partial
+
+API_TOKEN_KEY = "GITHUB_PAT"
+
 
 @click.command()
 @click.option(
     "-f",
     "--file",
     "filename",
-    default=None,
     help="File containing line-separated repository names",
 )
 @click.option(
     "-y", "skip_verify", is_flag=True, default=False, help="Skip delete verification"
 )
 def cli(filename, skip_verify):
-    pass
+    github_api = github.Api(getenv(API_TOKEN_KEY))
+
+    # define where the repo list should come from,
+    # file takes precedence over API
+    repo_list_source = None
+    if filename:
+        if isfile(filename):
+            repo_list_source = partial(list_repos_from_file, filename)
+        else:
+            raise Exception(f"'{filename}': No such file")
+    else:
+        repo_list_source = github_api.list_user_repos
+
+    # obtain the repositories to be deleted, potentially an API call
+    repositories = repo_list_source()
 
 
 if __name__ == "__main__":
