4ae35c64fd69b5d08d1e94113cf89ee22608b906
Faithfulness Alamu <vaguemail369@gmail.com>
2021-07-24 23:42:08 +0100

    feat(api): create a class for API calls

---
+++ api/github.py
@@ -0,0 +1,7 @@
+class Api:
+    def __init__(self, api_token):
+        self._api_token = api_token
+
+    def list_user_repos(self):
+        """list_user_repos makes an API call to retrieve repos for authed user"""
+        pass
