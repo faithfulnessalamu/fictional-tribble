f5a31efbe52d8e9eed1300be5b43b3251ddda403
Faithfulness Alamu <vaguemail369@gmail.com>
2021-07-25 11:52:32 +0100

    fix(util): ignore empty lines in file source

--- util.py
+++ util.py
@@ -1,5 +1,9 @@
 def list_repos_from_file(filename):
     repo_list = []
     with open(filename) as fd:
-        repo_list.extend(map(str.strip, fd.readlines()))
+        for line in fd.readlines():
+            line = line.strip()
+            if line:
+                repo_list.append(line)
+
     return repo_list
